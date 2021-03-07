64bb3ff13dd87c95755773e6caae0d7ea2222389
Andrey Gorshkov <megabgg@yandex.ru>
2021-03-07 23:01:10 +0300

    The second set of edits from the mentor #2

--- src/Games/brain-gcd.php
+++ src/Games/brain-gcd.php
@@ -16,11 +16,11 @@ function generateQuestionAndAnswer(): array
     $num1 = rand(1, 20);
     $num2 = rand(1, 20);
     $question = "{$num1} {$num2}";
-    $answer = getGcd($num1, $num2);
+    $answer = calculateGcd($num1, $num2);
     return [$question, $answer];
 }
 
-function getGcd(int $firstNum, int $secondNum): int
+function calculateGcd(int $firstNum, int $secondNum): int
 {
     return $firstNum % $secondNum === 0 ? $secondNum : calculateGcd($secondNum, $firstNum % $secondNum);
 }
