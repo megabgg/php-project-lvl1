967c0a83e75f64f35a1b06e7044ac5f319058317
Andrey Gorshkov <megabgg@yandex.ru>
2021-03-21 22:40:28 +0300

    Fix function getProgression in brain-progression game

--- src/Games/brain-progression.php
+++ src/Games/brain-progression.php
@@ -15,21 +15,20 @@ function generateQuestionAndAnswer(): array
 {
     $progressionStep = rand(1, 10);
     $progressionLength = rand(6, 10);
+    $startValueProgression = rand(1, 5);
+    $progression = getProgression($startValueProgression, $progressionStep, $progressionLength);
     $hiddenProgressionStep = rand(0, $progressionLength - 1);
-    $progression = getProgression($progressionStep, $progressionLength);
     $answer = $progression[$hiddenProgressionStep];
     $progression[$hiddenProgressionStep] = '..';
     $question = implode(" ", $progression);
     return [$question, $answer];
 }
 
-function getProgression(int $step, int $length): array
+function getProgression(int $startValue, int $step, int $length): array
 {
-    $currentCount = 0;
     $result = [];
-    for ($j = 0; $j < $length; $j++) {
-        $currentCount += $step;
-        $result[] = $currentCount;
+    for ($i = 0; $i < $length; $i++) {
+        $result[] = $startValue + $step * $i;
     }
     return $result;
 }
